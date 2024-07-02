42c1f349934ee012641ee72d395391ec045e9988
LesunenkoNikita <nikitoslesunenko@gmail.com>
2024-07-02 13:07:52 +0300

    added decrypt function

--- Assignment3/Assignment3/caesar.cpp
+++ Assignment3/Assignment3/caesar.cpp
@@ -8,3 +8,12 @@ char* encrypt(char* rawText, int key)
 	}
 	return rawText;
 }
+
+char* decrypt(char* encryptedText, int key)
+{
+	for (int i = 0; i < strlen(encryptedText); i++)
+	{
+		encryptedText[i] -= key;
+	}
+	return encryptedText;
+}
