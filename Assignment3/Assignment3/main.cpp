59ef9ea74ed508bf7f4297fdda3f4e7f1c807201
LesunenkoNikita <nikitoslesunenko@gmail.com>
2024-07-02 13:54:12 +0300

    main fix

--- Assignment3/Assignment3/main.cpp
+++ Assignment3/Assignment3/main.cpp
@@ -48,7 +48,8 @@ int main(void)
 			fgets(key_input, 16, stdin);
 			key_input[strcspn(key_input, "\n")] = '\0';
 			int key = atoi(input);
-			printf(encrypt(text, key)'\n');
+			printf(encrypt(text, key));
+			printf('\n');
 			continue;
 		}
 		else if (strcmp(input, "2") == 0)
@@ -62,7 +63,8 @@ int main(void)
 			fgets(key_input, 16, stdin);
 			key_input[strcspn(key_input, "\n")] = '\0';
 			int key = atoi(input);
-			printf(decrypt(text, key)'\n');
+			printf(decrypt(text, key));
+			printf('\n');
 			continue;
 		}
 		else
